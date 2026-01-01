e6fdbb8b5278ac85e218afe46de0cb04cb72567f
dhanasrinair06 <108911074+dhanasrinair06@users.noreply.github.com>
2026-01-01 16:43:11 +0000

    Deploying to gh-pages from @ dhanasrinair06/dhanasrinair06.github.io@673fb4e78d0d3a6e14ae29afbeee830d07649791 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "dhanasrinair06/dhanasrinair.github.io",
+    "data-repo": "dhanasrinair06/dhanasrinair06.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
